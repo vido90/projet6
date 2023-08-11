f5dfb2333bca73af028cb1ffbeadf6c0aab0e6e2
Vidova ASSIAKOLEY <vidovaa@outlook.com>
2023-08-12 00:05:30 +0200

    add filter

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -2,7 +2,8 @@
 
 
 const buildHtml = (projet)=>{
-    for (let i = 0; i < projet.length; i++) {
+    gallery.innerHTML = '';
+    for (let i = 0; i < projet.length; i++) {        
         let figure = document.createElement("figure")
         let image = document.createElement("img")
         let figcaption = document.createElement("figcaption")
@@ -16,11 +17,60 @@ const buildHtml = (projet)=>{
     }  
 }
 
-const buildFilterBtn()=>{
-    for()
+function filterDataByCategoryId(data, categoryId) {
+    if (categoryId === 0) {
+        return data;
+    } else {
+        return data.filter(element => element.category.id === categoryId);
+    }
 }
 
-let gallery = document.querySelector(".gallery")
+const buildFilterBtn = (categories)=>{
+    category.innerHTML = '';
+    let allButton = document.createElement('button'); //créer buttons pour afficher les catégories
+    allButton.textContent = 'Tous';
+    allButton.classList.add("buttons");
+    allButton.classList.add("active");
+    category.appendChild(allButton);
+    allButton.addEventListener('click', () => {
+        getAllWorks();
+        const activeButton = document.querySelector('.category-buttons button.active');
+            if (activeButton) {
+                activeButton.classList.remove('active');
+            }
+        allButton.classList.add('active');
+
+        const categoryButtons = document.querySelectorAll('.category-buttons button:not(:first-child)');
+        categoryButtons.forEach(button => button.classList.remove('active'));
+        
+    }
+    )
+
+    for (let i = 0; i < categories.length; i++) {
+        let oneButton = document.createElement('button');
+        oneButton.textContent = categories[i].name;
+        oneButton.classList.add("buttons");
+        oneButton.addEventListener('click', () => {
+            getFilterWorks(categories[i].id);
+            const activeButton = document.querySelector('.category-buttons button.active');
+            if (activeButton) {
+                activeButton.classList.remove('active');
+            }
+            oneButton.classList.add('active');
+
+            allButton.classList.remove('active');
+        }); 
+        category.appendChild(oneButton);
+
+    }
+
+
+    
+} 
+
+let gallery = document.querySelector(".gallery");
+let category = document.querySelector(".category-buttons");
+
 
 
 /*assync await permet de faire attendre le code d'après pour ne pas qu'il s'exécute en meme temps*/
@@ -38,21 +88,36 @@ const getAllWorks = async() => {
         })
    
 }
-/*  //renommer la fonction et changer le lien
-//const getAllWorks = async() => {
-    //fetch('http://localhost:5678/api/works')
-		//.then(rep => {
+
+const getFilterWorks = async(filterid) => {
+    fetch('http://localhost:5678/api/works')
+		.then(rep => {
 			const works = rep.json()
             return works
-		//})
+		})
         .then(rep => {
-            buildHtml(rep)   //renomer ça                          
+            const filteredData = filterDataByCategoryId(rep, filterid);
+            buildHtml(filteredData);                             
+                console.log(filteredData)   
+                
+        })
+   
+}
+//renommer la fonction et changer le lien
+
+const getCategories = async() => {
+    fetch('http://localhost:5678/api/categories')
+		.then(rep => {
+			const categories = rep.json()
+            return categories
+		})
+        .then(rep => {
+            buildFilterBtn(rep)   //renomer ça                          
                 console.log(rep)   
                 
         })
    
-} */
+} 
 getAllWorks()
-
-
+getCategories()
 
